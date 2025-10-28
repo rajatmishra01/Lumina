5f629976a62319e09ef6d1b9cfed18a1e143305f
Babuli <vermaanvee1@outlook.com>
2025-10-28 15:33:09 +0530

    updated file

--- Lumina/src/App.jsx
+++ Lumina/src/App.jsx
@@ -1,20 +1,19 @@
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginPage from './components/LoginPage';
-import SignupPage from "./components/SignupPage";
+import LoginPage from "./components/LoginPage";
 import Dashboard from "./components/Dashboard";
+import CreateAccountPage from "./components/CreateAccountPage"; //  signup page
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/" element={<LoginPage />} /> {/* Default: login */}
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/signup" element={<CreateAccountPage />} /> {/* signup */}
       </Routes>
     </Router>
-
-  );
-  
+ );
 }
 
-export default App
+export default App;
